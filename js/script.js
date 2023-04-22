23af0c0403db545557fb4cee7acce686d6cddc96
Nikolay <Koliansssss2014@mail.ru>
2023-04-22 14:53:29 +0300

    Create timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', ()=>{
-
+    //Create tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabContent = document.querySelectorAll('.tabcontent'),
           tabParent = document.querySelector('.tabheader__items');
@@ -35,4 +35,64 @@ window.addEventListener('DOMContentLoaded', ()=>{
             })
         }
     })
+    //Create timer
+
+    const deadline = '2023-03-28';
+    const date = new Date();
+    date.setDate(date.getDate() + 2);
+    // date.setHours(date.getHours() - 1);
+    // date.setMinutes(date.getMinutes() - 1);
+    date.setSeconds(date.getSeconds() - 1);
+    const deadlineRemaining = date;
+
+    function getTimeRemaining(endTime){
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        
+        if(t <= 0 ){
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }else{
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t/1000) % 60); 
+        }
+        
+        return {
+            total: t,
+            'days' : days,
+            'hours'  : hours,
+            'minutes': minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(selector, endTime){
+        const timer =document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds');
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRemaining(endTime);
+
+            days.textContent = (t.days < 10)? `0${t.days}`: t.days; //либо сделать функцию которая будет принимать аргумент  t.days и проверять его меньше ли оно 10
+            hours.textContent = (t.hours < 10)? `0${t.hours}`: t.hours;
+            minutes.textContent = (t.minutes < 10)? `0${t.minutes}`: t.minutes;
+            seconds.textContent = (t.seconds < 10)? `0${t.seconds}`: t.seconds;
+
+            if(t.total <=0){
+                clearInterval(timeInterval)
+            }
+        }
+    }
+    setClock('.timer', deadlineRemaining);
+
 });
