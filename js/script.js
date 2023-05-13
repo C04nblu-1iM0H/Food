20cb45fe07c47518b90c7025b8ce8bf09b64cda2
Nikolay <Koliansssss2014@mail.ru>
2023-05-13 15:13:30 +0300

    Отправка данных FormData\JSON

--- js/script.js
+++ js/script.js
@@ -237,4 +237,60 @@ window.addEventListener('DOMContentLoaded', ()=>{
         '16', 
         '.menu .container'
         ).render();
+
+    
+    //Forms
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading : 'Загрузка...',
+        success : 'Спасибо, скоро мы с вами свяжемся',
+        failure : 'Что-то пошло не так...'
+    };
+
+    forms.forEach((form) => {
+        postData(form);
+    })
+
+    function postData(form){
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            
+            const inputs = document.querySelectorAll('input');
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage); 
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            //При использовании FormData с XMLHttpRequest заголовок setRequestHeader использовать не нужно, за исключением json 
+            request.setRequestHeader('Content-Type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            })
+
+            const json = JSON.stringify(object);
+
+            request.send(json); //либо вместо json передать formData
+
+            request.addEventListener('load', ()=>{
+                if(request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    // inputs.forEach(input => input.value = '');
+                    form.reset();
+                    setTimeout(()=>{
+                        statusMessage.remove();
+                    }, 2000);
+                }else{
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        })
+
+    }
 });
